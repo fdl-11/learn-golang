e28bb35d892db1bb625890ab6dab8a3b3db67ab4
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-14 07:59:36 +0700

    lebih ringkas

--- belajar-golang/27_function-as-parameter.go
+++ belajar-golang/27_function-as-parameter.go
@@ -11,45 +11,29 @@ import "strings"
 type Filter func(string) string
 
 func sayHelloWithFilter(name string, filter Filter) string {
-	nameFiltered := filter(name)
-
-	return nameFiltered
+	return filter(name)
 }
 
 func spamFilter(name string) string {
 	if name == "Anjing" {
 		return "Hayoloooo"
-	} else {
-		return name
 	}
+
+	return name
 }
 
 func upperFilter(name string) string {
-	nameFiltered := strings.ToUpper(name)
-	return nameFiltered
+	return strings.ToUpper(name)
 }
 
 func main() {
-	name1 := "Fadli"
-	name2 := "Anjing"
+	names := []string{"Fadli", "Anjing"}
 
-	// Original name
-	fmt.Println(name1)
-	fmt.Println(name2)
-	
-	fmt.Println()
-	
-	// SpamFilter
-	name1 = sayHelloWithFilter(name1, spamFilter)
-	name2 = sayHelloWithFilter(name2, spamFilter)
-	
-	fmt.Println(name1)
-	fmt.Println(name2)
-	
-	fmt.Println()
-
-    // UpperFilter
-	fmt.Println(sayHelloWithFilter(name1, upperFilter))
-	fmt.Println(sayHelloWithFilter(name2, upperFilter))
+	for _, name := range names {
+		fmt.Println(name)
+		fmt.Println(sayHelloWithFilter(name, spamFilter))
+		fmt.Println(sayHelloWithFilter(sayHelloWithFilter(name, spamFilter), upperFilter))
+		fmt.Println()
+	}
 
 }

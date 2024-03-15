b08b6fcacf3cc809515c76cea91cab07dd9de44a
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-15 08:40:50 +0700

    closure

---
+++ belajar-golang/30_closure.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Fadli"
+	counter := 0
+
+	increment := func() {	// Anonymous function
+		name := "Darusalam"	// Agar tidak mereplace variabel name di atas, maka perlu deklarasi ulang atau bedakan nama
+		fmt.Println("Increment")
+		counter++
+		fmt.Println(name)
+	}
+
+	increment()
+	increment()
+
+	fmt.Println(counter)
+	fmt.Println(name)
+}

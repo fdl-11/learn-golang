ff25d8490783cf5dbbc54d5886afab32f0f7e125
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-16 21:22:53 +0700

    add comment

--- belajar-golang/30_closure.go
+++ belajar-golang/30_closure.go
@@ -7,7 +7,7 @@ func main() {
 	counter := 0
 
 	increment := func() {	// Anonymous function
-		name := "Darusalam"	// Agar tidak mereplace variabel name di atas, maka perlu deklarasi ulang atau bedakan nama
+		name := "Darusalam"	// Agar tidak mereplace variabel name yg sudah ada di luar, maka perlu deklarasi ulang atau bedakan nama
 		fmt.Println("Increment")
 		counter++
 		fmt.Println(name)

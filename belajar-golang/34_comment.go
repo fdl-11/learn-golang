d84a4a863a6eb3e0c4e656b71d7bbd651341b975
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-18 08:57:23 +0700

    comment

---
+++ belajar-golang/34_comment.go
@@ -0,0 +1,9 @@
+package main
+
+/**
+Ini komentar multi line
+Biasanya di luar block function, untuk menjelaskan functionnya
+*/
+func main() {
+	// Ini komentar single line, biasanya di dalam block function
+}

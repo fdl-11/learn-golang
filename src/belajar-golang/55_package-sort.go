2268f20e2abeb2fd22973a2f218fc77062b6bb63
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-24 14:24:05 +0700

    package sort

---
+++ src/belajar-golang/55_package-sort.go
@@ -0,0 +1,42 @@
+/*
+Package sort adalah package yg berisikan utilitas untuk proses pengurutan
+Agar data bisa diurutkan, kita harus mengimplementasikan kontrak di interface sort.interface
+*/
+
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+type UserSlice []User
+
+func (value UserSlice) Len() int {
+	return len(value)
+}
+
+func (value UserSlice) Less(i, j int) bool {
+	return value[i].Age < value[j].Age
+}
+
+func (value UserSlice) Swap(i, j int) {
+	value[i], value[j] = value[j], value[i]
+}
+
+func main() {
+	users := []User {
+		{"Fadli", 21},
+		{"Cahyo", 23},
+		{"Wina", 22}, 
+		{"Dinda", 24},
+	}
+
+	sort.Sort(UserSlice(users))
+	fmt.Println(users)
+}

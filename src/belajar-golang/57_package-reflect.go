b087bf6271fad054bfc4a3c10da4d3ab40504aff
fdl-11 <fadlidarusalam11@gmail.com>
2024-03-25 10:00:07 +0700

    validation library with reflecttion

--- src/belajar-golang/57_package-reflect.go
+++ src/belajar-golang/57_package-reflect.go
@@ -15,6 +15,27 @@ type Sample struct {
 	Name string `required:"true" max:"10"`
 }
 
+type COntohLagi struct {
+	Name string
+	Description string
+}
+
+// validasi dengan reflection
+func IsValid(data interface{}) bool {
+	t := reflect.TypeOf(data)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("required") == "true" {
+			// return reflect.ValueOf(data).Field(i).Interface() != ""
+			value := reflect.ValueOf(data).Field(i).Interface()
+			if value == "" {
+                return false
+            }
+		}
+	}
+	return true
+}
+
 func main() {
 	sample := Sample{"Fadli"}
 
@@ -24,4 +45,12 @@ func main() {
 	fmt.Println(sampleType.Field(0).Name)
 	fmt.Println(sampleType.Field(0).Tag.Get("required"))
 	fmt.Println(sampleType.Field(0).Tag.Get("max"))
+
+	fmt.Println()
+	
+	sample.Name = ""
+	fmt.Println(IsValid(sample))
+
+	contoh := COntohLagi{"Fadli", "Darusalam"} // true karena tidak ada tag
+	fmt.Println(IsValid(contoh))
 }
